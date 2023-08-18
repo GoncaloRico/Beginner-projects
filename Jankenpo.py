5566c53e53ba8a89e93b7988b294ea987df7df5b
GoncaloRico <gmkkrico@gmail.com>
2023-08-18 16:59:56 +0100

    Optimized Jankenpo
    Optimized code and fixed some bugs

--- Jankenpo.py
+++ Jankenpo.py
@@ -1,38 +1,46 @@
 import random
-#r>s s>p p>r
+outcome = {"r":"Scissors", "p":"Rock", "s":"Paper"} #This dictionary contains all the pairs of win conditions
+play_again = "y"
+score_user = 0
+score_computer = 0
+user = ""
 
 def play():
-    stop = False
     play_again = "y"
     score_user = 0
     score_computer = 0
     user = ""
-    while stop == False:
+    while play_again == "y":
         user = input("Select Rock (R), Paper (P) or Scissors (S): ").lower()
-        computer = random.choice(["r", "p", "s"])
-        if user == computer:
-            print("It's a tie!")
-            play_again = input(f"Current score is: You {score_user} - {score_computer} Computer. Do you want to play again? Pick Yes (Y) or No (N): ").lower()
+        computer = random.choice(["Rock", "Paper", "Scissors"])
 
-        if user != "r" and user != "p" and user != "s":
+        if user not in {"r", "p", "s"}:
             print("Invalid choice! Please type 'R', 'P' or 'S'!")
+            continue
 
-        if (user == "r" and computer == "s") or (user == "p" and computer == "r") or (user == "s" and computer == "p"):
-            print(f"Congratulations, you win! Computer picked scissors.")
-            score_user = score_user + 1
+        elif user == computer:
+            print("It's a tie!")
+
+        elif outcome[user] == computer: #This will match up the user's input with it's correspondence in our "outcome dictionary". 
+                                        #For example, if the user picks Rock (r), it's corresponding element in "outcome" would be "Scissors". If that's what the computer picked, it's a win for the user!
+            print(f"Congratulations, you win! Computer picked {computer}.")
+            score_user += 1
+
+        else:
+            print(f"Aw shucks, you lose! Computer picked {computer}.")
+            score_computer += 1
+
+        while True:
             play_again = input(f"Current score is: You {score_user} - {score_computer} Computer. Do you want to play again? Pick Yes (Y) or No (N): ").lower()
 
-        if (user == "r" and computer == "p") or (user == "p" and computer == "s") or (user == "s" and computer == "r"):
-            print(f"Aw shucks, you lose! Computer picked paper.")
-            score_computer = score_computer + 1
-            play_again = input(f"Current score is: You {score_user} - {score_computer} Computer. Do you want to play again? Pick Yes (Y) or No (N): ").lower()
-
-        if play_again != "n" and play_again != "y":
+            if play_again == "n":
+                print(f"Thank you for playing! The final score is: You {score_user} - {score_computer} Computer! Come back for a rematch!")
+                break
+            
+            elif play_again == "y":
+                break
+            
             print("Invalid choice! Please type 'Y' or 'N'!")
-
-        if play_again == "n":
-            print(f"Thank you for playing! The final score is: You {score_user} - {score_computer} Computer! Come back for a rematch!")
-            stop = True
 
 play() 
 
